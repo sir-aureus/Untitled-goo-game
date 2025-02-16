7ee42c4a572f071cfb2972349e92f31ddbe4a041
Gyokou <leonardo.rodri.bueno@gmail.com>
2025-02-16 15:07:04 -0300

    + imported the goo's eye sprites + created the eye objects and animations

---
+++ Assets/Scripts/EyeAnimation.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class EyeAnimation : MonoBehaviour
+{
+    [SerializeField] private Sprite[] _defaultSprite;
+    
+    private float _nextCallTime;
+
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        InitializeAnimationParameters((int)Random.Range(0f, 5f)); //temp
+    }
+
+    public void InitializeAnimationParameters(int eyeType)
+    {
+        _spriteRenderer.sprite = _defaultSprite[eyeType];
+        _animator.SetInteger("Variation", eyeType);
+    }
+
+    public void Update()
+    {
+        if(Time.time >= _nextCallTime)
+        {
+            SetTrigger("Animation");
+            _nextCallTime = Time.time + Random.Range(5f, 10f);
+        }
+    }
+
+    private void SetTrigger(string paramName)
+    {
+        _animator.SetTrigger(paramName);
+    }
+}
